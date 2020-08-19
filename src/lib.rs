7d2fda33b6f84e666b674ad527af068d16351158
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2020-08-19 16:53:53 -0700

    draw some smileys

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use wasm_bindgen::prelude::*;
-
+use std::f64;
+use wasm_bindgen::JsCast;
 // Called when the wasm module is instantiated
 #[cfg(not(test))]
 #[wasm_bindgen(start)]
@@ -11,11 +12,53 @@ pub fn main() -> Result<(), JsValue> {
     let body = document.body().expect("document should have a body");
 
     // Manufacture the element we're gonna append
-    let val = document.create_element("p")?;
-    val.set_inner_html("Hello from Rust!");
+    let val = document.create_element("div")?;
+
+    val.set_inner_html("Hello from Rust2");
 
     body.append_child(&val)?;
 
+    let canvas = document.create_element("canvas")?;
+    canvas.set_attribute("height", "150")?;
+    canvas.set_attribute("width", "150")?;
+    body.append_child(&canvas)?;
+    //let canvas = document.get_element_by_id("canvas").unwrap();
+    let canvas: web_sys::HtmlCanvasElement = canvas
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .map_err(|_| ())
+        .unwrap();
+
+    let context = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
+
+    context.begin_path();
+
+    // Draw the outer circle.
+    context
+        .arc(75.0, 75.0, 50.0, 0.0, f64::consts::PI * 2.0)
+        .unwrap();
+
+    // Draw the mouth.
+    context.move_to(110.0, 75.0);
+    context.arc(75.0, 75.0, 35.0, 0.0, f64::consts::PI).unwrap();
+
+    // Draw the left eye.
+    context.move_to(65.0, 65.0);
+    context
+        .arc(60.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
+        .unwrap();
+
+    // Draw the right eye.
+    context.move_to(95.0, 65.0);
+    context
+        .arc(90.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
+        .unwrap();
+
+    context.stroke();
     Ok(())
 }
 
